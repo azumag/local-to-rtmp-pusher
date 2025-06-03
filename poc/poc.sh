62f449e683f20bc2f7de586d85f94c61dbf124ae
azumag <tsubasa.azumagakito@gmail.com>
2025-06-04 08:00:03 +0900

    feat: FFmpeg設定を追加し、RTMP配信のビットレート制限を設定

--- poc/poc.sh
+++ poc/poc.sh
@@ -5,3 +5,12 @@ ffmpeg -i "udp://127.0.0.1:1234?timeout=0" -c copy -f flv rtmp://localhost:1936/
 ffmpeg -re -i videoA.mp4 -c copy -f mpegts udp://127.0.0.1:1234
 
 ffmpeg -re -i videoB.mp4 -c copy -f mpegts udp://127.0.0.1:1234
+
+
+# 2500kに制限、vrcdnなどのRTMP配信に近い設定
+# ffmpeg -re -stream_loop -1 -i ./videos/standby.mp4 \
+ffmpeg -i "udp://127.0.0.1:1234?timeout=0" \
+  -c:v libx264 -preset veryfast \
+  -b:v 2500k -maxrate 2500k -bufsize 5000k \
+  -c:a aac -b:a 128k \
+  -f flv rtmp://xxxxxxxx
